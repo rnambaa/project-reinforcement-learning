bd5eecff54add276d9d1101fd0b69fa21cd15734
Tim Mooren <timmooren9@gmail.com>
2024-01-10 18:59:35 +0100

    Update SmartGridBatteryEnv class with new constants and methods

--- utils.py
+++ utils.py
@@ -54,11 +54,11 @@ class SmartGridBatteryEnv(gymnasium.Env):
         super(SmartGridBatteryEnv, self).__init__()
 
         # Constants
-        self.max_battery_capacity = 50  # kWh
-        self.efficiency = 0.9
-        self.max_power = 25  # kW
-        self.min_required_battery = 20  # kWh
-        self.charge_discharge_limit = 20  # kWh
+        self.BATTERY_CAPACITY = 50  # kWh
+        self.EFFICIENCY = 0.9
+        self.MAX_POWER = 25  # kW/h
+        self.MIN_BATTERY = 20  # kWh
+        self.DAY_USAGE = 20  # kWh
 
         # variables
         self.car_available = True
@@ -66,13 +66,13 @@ class SmartGridBatteryEnv(gymnasium.Env):
         # State Space: Battery charge level, Time of day, Electricity prices
         self.observation_space = spaces.Box(
             low=np.array([0, 0, 0]),
-            high=np.array([self.max_battery_capacity, 24, np.inf]),
+            high=np.array([self.BATTERY_CAPACITY, 24, np.inf]),
             dtype=np.float32,
         )
 
         # Action Space: Charge/Discharge amount (positive for charging, negative for discharging)
         self.action_space = spaces.Box(
-            low=-self.max_power, high=self.max_power, shape=(1,), dtype=np.float32
+            low=-self.MAX_POWER, high=self.MAX_POWER, shape=(1,), dtype=np.float32
         )
 
         # Initialize state and data
@@ -85,13 +85,22 @@ class SmartGridBatteryEnv(gymnasium.Env):
     @staticmethod
     def adjust_battery_for_availability(self, battery_level: float, hour: int):
         """Adjust the battery level if the car is not available (being used)) between 8am and 6pm"""
-        if hour >= 8 and hour < 18 and not self.car_available:
-            battery_level -= 20
+        if hour == 18 and not self.car_available:
+            battery_level -= self.DAY_USAGE
         return battery_level
 
+    def update_state(self, battery_level: float):
+        self.current_index = (self.current_index + 1) % len(self.data)
+        new_hour = self.data.at[self.current_index, "Hour"]
+        new_price = self.data.at[self.current_index, "Price"]
+        # Update state
+        self.state = [battery_level, new_hour, new_price]
+
     def step(self, action: float):
+        action = action[0]
         # Extract state components
-        battery_level, hour, _ = self.state
+        battery_level, hour, price = self.state
+        reward = 0
 
         # update the car availability randomly every day
         if hour == 1:
@@ -100,29 +109,35 @@ class SmartGridBatteryEnv(gymnasium.Env):
         # Adjust battery level if the car is not available
         battery_level = self.adjust_battery_for_availability(self, battery_level, hour)
 
-        # Calculate new battery level considering efficiency
-        delta_energy = action * self.efficiency if self.car_available else 0
-        new_battery_level = max(
-            min(battery_level + delta_energy, 0), self.max_battery_capacity
-        )
+        # Calculate new battery level if car is available or if it is outside the 8am-6pm window
+        if self.car_available or (hour < 8 or hour > 18):
+            # Calculate new battery level considering efficiency
+            delta_energy = action * self.EFFICIENCY
+            if delta_energy > 0:
+                # charging
+                max_possible_charging_amount = self.MAX_POWER - battery_level
+                charging_amount = min(max_possible_charging_amount, delta_energy)
+                battery_level += charging_amount
+                # pay the price to charge the battery
+                reward -= 2 * price * charging_amount / self.EFFICIENCY
+            else:
+                # discharging
+                max_possible_discharging_amount = -battery_level
+                discharging_amount = max(max_possible_discharging_amount, delta_energy)
+                battery_level += discharging_amount
+                # get paid the price to discharge the battery
+                reward += price * discharging_amount / self.EFFICIENCY
 
         # ensure the battery level is at least 20kWh at 7am
-        if hour == 7 and new_battery_level < 20:
-            difference = 20 - new_battery_level
+        if hour == 7 and battery_level < 20:
+            charge_needed = 20 - battery_level
             # pay the difference in price to charge the battery
-            delta_energy += difference
-            new_battery_level = 20
+            reward -= 2 * price * charge_needed / self.EFFICIENCY
 
-        # Update to the next hour in the dataset
-        self.current_index = (self.current_index + 1) % len(self.data)
-        new_hour = self.data.at[self.current_index, "Hour"]
-        new_price = self.data.at[self.current_index, "Price"]
+            battery_level = 20
 
-        # Calculate reward (or cost)
-        reward = -new_price * delta_energy
-
-        # Update state
-        self.state = [new_battery_level, new_hour, new_price]
+        # update the state
+        self.update_state(battery_level)
 
         # Check if the episode is done (e.g., end of the dataset)
         done = self.current_index == 0
@@ -135,7 +150,7 @@ class SmartGridBatteryEnv(gymnasium.Env):
         self.current_index = 0
         # state = [battery_level, hour, price]
         self.state = [
-            self.max_battery_capacity / 2,
+            self.BATTERY_CAPACITY / 2,
             self.data.at[self.current_index, "Hour"],
             self.data.at[self.current_index, "Price"],
         ]
